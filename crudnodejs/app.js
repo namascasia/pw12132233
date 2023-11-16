a9708c5957cfdbedc4a81eb31f9604a0dbfc5c3a
namascasia <namascasia@gmail.com>
2023-11-16 12:55:03 -0700

    Obtener maestros y maestro

--- crudnodejs/app.js
+++ crudnodejs/app.js
@@ -18,12 +18,35 @@ conexion.connect( (error)=>{
     console.log("Conectado a la base de datos");
 });
 
+//on server
+app.listen("3000", ()=>{
+    console.log("Servidor puerto 3000");
+});
 
 app.get("/", (req, res)=>{
     res.send("<h1> Ruta inicio </h1>")
 });
+// app.put();
+// app.post();
+// app.delete();
 
-//on server
-app.listen("3000", ()=>{
-    console.log("Servidor puerto 3000");
+//Mostrar todos los maestros
+app.get('/api/maestros', (req, res)=>{
+    conexion.query('select * from maestros', (error,filas)=>{
+        if(error) throw error;
+        res.send(filas);
+    });
+});
+
+//Mostrar un solo maestro
+app.get('/api/maestros/:id', (req, res)=>{
+    // conexion.query("select * from maestros where clave = ? LIMIT 1", [req.params.id], (error, fila)=>{
+    //         if(error) throw error;
+    //         res.send(fila);
+    // }); 
+    const { id } = req.params;
+    conexion.query(`select * from maestros where clave = ${id} LIMIT 1`, (error,filas)=>{
+        if(error) throw error;
+        res.send(filas);
+    });
 });
